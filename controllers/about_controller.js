96e7936b5b3f67fe953d708e577fb5b2c17caac9
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-08-10 14:41:32 +0530

    CRUD on about us component

--- controllers/about_controller.js
+++ controllers/about_controller.js
@@ -1,21 +1,72 @@
 const About = require('../models/about');
 
-exports.getAbout = (req,res)=>{
-    res.send("About route");
+exports.getAbout = async (req,res)=>{
+    try {
+        const about = await About.find({});
+        return res.json(200,{
+            message:about
+        })
+    } catch (error) {
+        return res.json(500,{
+            message:error.message
+        })
+    }
 }
 
-exports.postAbout = (req,res)=>{
-    res.send("About post route");
+exports.postAbout = async (req,res)=>{
+    try {
+        const {about} = req.body;
+        const newAbout = new About({about})
+        await newAbout.save();
+        return res.json(200,{
+            message:newAbout
+        })
+    } catch (error) {
+        return res.json(500,{
+            message:error.message
+        })
+    }
 }
 
-exports.getSpecificAbout = (req,res)=>{
-    res.send("About specific route");
+exports.getSpecificAbout = async (req,res)=>{
+    try {
+        const about = await About.findById(req.params.id);
+        return res.json(200,{
+            about
+        })
+    } catch (error) {
+        return res.json(500,{
+            message:error.message
+        })
+    }
 }
 
-exports.updateAbout = (req,res)=>{
-    res.send("About route");
+exports.updateAbout = async (req,res)=>{
+    try {
+        const {about} = req.body;
+        const newAbout = await About.findByIdAndUpdate(req.params.id,{about});
+        newAbout.save();
+        res.json(200,{
+            newAbout
+        })
+    } catch (error) {
+        res.json(500,{
+            message:error.message
+        })
+    }
+
 }
 
-exports.deleteAbout = (req,res)=>{
-    res.send("About route");
+exports.deleteAbout = async (req,res)=>{
+   try {
+    const about = await About.findById(req.params.id);
+    about.remove();
+    res.json(200,{
+     message:"About us line deleted"
+    })
+   } catch (error) {
+    res.json(500,{
+        message:error.message
+    })
+   }
 }
--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.use(express.json());
 app.use(cors());
 app.use('/',require('./routes/about'))
 app.listen(process.env.PORT,()=>{
-    console.log("Listening on port 3000");
+    console.log("Listening on port 5000");
 })
