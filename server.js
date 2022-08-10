be258f5564c9416d9ae7b3cd92062cddbc5b159d
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-08-10 10:41:43 +0530

    Database connection

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const env = require('dotenv').config();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+//mongodb connection
+mongoose.connect(process.env.MONGO_URL);
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,"Error in connecting database"));
+db.once('open',()=>console.log("Connected to database"));
 //Using middlewares
 app.use(express.json());
 app.use(cors());
