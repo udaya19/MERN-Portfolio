4a2373cf40560fe3b95ee36213283eb3834af9eb
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-07 08:49:34 +0530

    NavBar component html css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,27 @@
 import './App.css';
-import Header from './components/HomePages/Header';
-import About from './components/HomePages/About';
-import Contact from './components/HomePages/Contact';
-import Education from './components/HomePages/Education';
-import Experience from './components/HomePages/Experience';
-import Footer from './components/HomePages/Footer';
-import Login from './components/HomePages/Login';
+// import Header from './components/HomePages/Header';
+// import About from './components/HomePages/About';
+// import Contact from './components/HomePages/Contact';
+// import Education from './components/HomePages/Education';
+// import Experience from './components/HomePages/Experience';
+// import Footer from './components/HomePages/Footer';
+// import Login from './components/HomePages/Login';
 import NavBar from './components/HomePages/NavBar';
-import Projects from './components/HomePages/Projects';
+// import Projects from './components/HomePages/Projects';
 //Admin component
-import Admin from './components/AdminComponents/Admin';
+// import Admin from './components/AdminComponents/Admin';
 //Edit components
 
-import EditAbout from './components/EditComponents/EditAbout';
-import EditEducation from './components/EditComponents/EditEducation';
-import EditExperience from './components/EditComponents/EditExperience';
-import EditProjects from './components/EditComponents/EditProjects';
-
+// import EditAbout from './components/EditComponents/EditAbout';
+// import EditEducation from './components/EditComponents/EditEducation';
+// import EditExperience from './components/EditComponents/EditExperience';
+// import EditProjects from './components/EditComponents/EditProjects';
+import {Routes,Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      App
+     {/* NavBar component */}
+     <NavBar />
     </div>
   );
 }
--- frontend/src/components/HomePages/NavBar.jsx
+++ frontend/src/components/HomePages/NavBar.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './styles/navbar.module.css';
 const NavBar = () => {
     return ( 
     <div>
-        NavBar
+        <div className={styles.navContainer}>
+            <nav>
+                <div className={styles.logoBtn}>
+                    <Link to='/'>
+                        <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className={styles.logoImg} alt="logo" />
+                    </Link>
+                    <div className='btn'>
+                        <div className={styles.bar1}></div>
+                        <div className={styles.bar2}></div>
+                        <div className={styles.bar3}></div>
+                    </div>
+                </div>
+                <div className={styles.linksContainer}>
+                    <ul className={styles.links}>
+                            <li><Link to='/' >Home</Link></li> 
+                            <li><Link to='/' >About</Link></li> 
+                            <li><Link to='/' >Education</Link></li> 
+                            <li><Link to='/' >Experience</Link></li> 
+                            <li><Link to='/' >Projects</Link></li> 
+                            <li><Link to='/' >Contact</Link></li> 
+                            <li><Link to='/' >Home</Link></li> 
+                            <li className={styles.adminLi} ><Link to='/' >Admin</Link></li> 
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </div> 
     );
 }
