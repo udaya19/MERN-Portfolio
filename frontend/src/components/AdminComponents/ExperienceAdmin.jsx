701c8602563042e8debf28a850bc494d554fcf0e
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-10 10:47:53 +0530

    ExperienceAdmin component and changes in experience component

--- frontend/src/components/AdminComponents/ExperienceAdmin.jsx
+++ frontend/src/components/AdminComponents/ExperienceAdmin.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React from "react";
+import styles from "./styles/admin.module.css";
+import { Link } from "react-router-dom";
 const ExperienceAdmin = () => {
-    return ( 
-    <div>
-        ExperienceAdmin
-    </div> 
-    );
-}
- 
+  return (
+    <div className={styles.sameComponent}>
+      <div className={styles.sameForm}>
+        <form action="">
+          <h4>Experience component</h4>
+          <label htmlFor="experience">Experience</label>
+          <input type="text" />
+          <button type="submit">Add Item</button>
+        </form>
+      </div>
+      <div className={styles.sameItem}>
+        <div className={styles.aboutInfo}>
+          <div className={styles.sameAdmin}>
+            <div className={styles.icons}>
+              <Link to="/editExperience">
+                <span className={styles.editIcon}>
+                  <i className="fas fa-edit"></i>
+                </span>
+              </Link>
+              <span className={styles.deleteIcon}>
+                <i className="fas fa-trash"></i>
+              </span>
+            </div>
+            <div className={styles.singleExperience}>
+              <p>
+                Worked as a full stack developer intern at Spotflock
+                Technologies, Hyderabad from 30th May 2022 to 15th July 2022.
+              </p>
+            </div>
+            {/* <h3 className={styles.itemDeleteTab}></h3> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default ExperienceAdmin;
--- frontend/src/components/HomePages/Experience.jsx
+++ frontend/src/components/HomePages/Experience.jsx
@@ -1,31 +1,21 @@
-import React from 'react';
-import styles from './styles/experience.module.css'
+import React from "react";
+import styles from "./styles/experience.module.css";
 const Experience = () => {
-    return ( 
-    <div className={styles.mainContainer} >
-        <div className={styles.experience}>
-            <h2 className={styles.title} >
-                Experience
-            </h2>
-            <div className={styles.experienceInfo}>
-                <div className={styles.experienceInfoImage}>
-                    <img 
-                        src="https://media-exp1.licdn.com/dms/image/C510BAQHVRo-fARASEw/company-logo_200_200/0/1528631690276?e=1670457600&v=beta&t=CThIIWH-VkgJA-L-Hr72Pl9Uz2Od3Lh8axszTksA9XU" 
-                        alt="logo" 
-                    />
-                </div>
-                <div className={styles.experienceInfoCompanyName}>
-                    <h3>Spotflock Technologies, Hyderabad India</h3>
-                        <p className={styles.experienceInfoDetails}>
-                            <b className={styles.detailsHeading}>Role:</b>Full Stack Developer Intern <br />
-                            <b className={styles.detailsHeading}>Duration:</b>30th May 2022 to 15th July 2022 <br />
-                            <b className={styles.detailsHeading}>Work Done:</b>Bug fixes in angular website and also built a static webpage
-                        </p>
-                </div>
-            </div>
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.experience}>
+        <h2 className={styles.title}>Experience</h2>
+        <div className={styles.experienceInfo}>
+          <div className={styles.experienceInfoCompanyName}>
+            <p className={styles.experienceInfoDetails}>
+              Worked as a full stack developer intern at Spotflock Technologies,
+              Hyderabad from 30th May 2022 to 15th July 2022.
+            </p>
+          </div>
         </div>
-    </div> 
-    );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Experience;
