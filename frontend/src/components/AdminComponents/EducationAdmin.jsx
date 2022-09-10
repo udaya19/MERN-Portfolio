b25bbb7832d13dbb5a0d02b23955372d96f1d918
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-10 09:58:21 +0530

    Project and Education component

--- frontend/src/components/AdminComponents/EducationAdmin.jsx
+++ frontend/src/components/AdminComponents/EducationAdmin.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React from "react";
+import styles from "./styles/admin.module.css";
+import { Link } from "react-router-dom";
 const EducationAdmin = () => {
-    return ( 
-    <div>
-        EducationAdmin
-    </div> 
-    );
-}
- 
+  return (
+    <div className={styles.sameComponent}>
+      <div className={styles.sameForm}>
+        <form>
+          <h4>Education Component</h4>
+          <label htmlFor="text">Education</label>
+          <input type="text" />
+          <button type="submit">Add Items</button>
+        </form>
+      </div>
+      <div className={styles.sameItem}>
+        <div className={styles.aboutInfo}>
+          <div className={styles.sameAdmin}>
+            <div className={styles.icons}>
+              <Link to="/editEducation">
+                <span className={styles.editIcon}>
+                  <i className="fas fa-edit"></i>
+                </span>
+              </Link>
+              <span className={styles.deleteIcon}>
+                <i className="fas fa-trash"></i>
+              </span>
+            </div>
+          </div>
+          <div className={styles.singleEducation}>
+            <p>Web development</p>
+          </div>
+          {/* <h3 className={styles.itemDeleteTab}>The message</h3> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default EducationAdmin;
