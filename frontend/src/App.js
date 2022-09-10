404334959b647bf6cd22b26c01ccf36f58e1bb71
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-10 15:16:24 +0530

    NavBar scroll functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,60 +1,62 @@
-import './App.css';
-import Header from './components/HomePages/Header';
-import About from './components/HomePages/About';
+import "./App.css";
+import Header from "./components/HomePages/Header";
+import About from "./components/HomePages/About";
 // import Contact from './components/HomePages/Contact';
-import Education from './components/HomePages/Education';
-import Experience from './components/HomePages/Experience';
-import Footer from './components/HomePages/Footer';
-import Login from './components/HomePages/Login';
-import NavBar from './components/HomePages/NavBar';
-import Projects from './components/HomePages/Projects';
+import Education from "./components/HomePages/Education";
+import Experience from "./components/HomePages/Experience";
+import Footer from "./components/HomePages/Footer";
+import Login from "./components/HomePages/Login";
+import NavBar from "./components/HomePages/NavBar";
+import Projects from "./components/HomePages/Projects";
 //Admin component
-import Admin from './components/AdminComponents/Admin';
+import Admin from "./components/AdminComponents/Admin";
 //Edit components
 
-import EditAbout from './components/EditComponents/EditAbout';
-import EditEducation from './components/EditComponents/EditEducation';
-import EditExperience from './components/EditComponents/EditExperience';
-import EditProjects from './components/EditComponents/EditProjects';
-import {Routes,Route} from 'react-router-dom';
+import EditAbout from "./components/EditComponents/EditAbout";
+import EditEducation from "./components/EditComponents/EditEducation";
+import EditExperience from "./components/EditComponents/EditExperience";
+import EditProjects from "./components/EditComponents/EditProjects";
+import { Routes, Route } from "react-router-dom";
+import { Element } from "react-scroll";
 function App() {
   return (
     <div className="App">
-     {/* NavBar component */}
-     <NavBar />
-     {/* <Header /> */}
-     <Routes>
-        <Route exact path='/' element={<Header />}/>
-     </Routes>
-     <Routes>
-        <Route exact path='/' element={<About />}/>
-     </Routes>
-     <Routes>
-        <Route exact path='/' element={<Education />}/>
-     </Routes>
-     <Routes>
-        <Route exact path='/' element={<Projects />}/>
-     </Routes>
-     <Routes>
-        <Route exact path='/' element={<Experience />}/>
-     </Routes>
-     {/* <Routes>
-        <Route exact path='/' element={<Footer />}/>
-     </Routes> */}
-     <Routes>
-        <Route exact path='/login' element={<Login />}/>
-     </Routes>
-     <Routes>
-        <Route exact path='/admin' element={<Admin />}/>
-     </Routes>
-     <Routes>
-      <Route path='/edit/:id' element={<EditAbout />} />
-      <Route path='/editEducation/:id' element={<EditEducation />} />
-      <Route path='/editProjects/:id' element={<EditProjects/>} />
-      <Route path='/editExperience/:id' element={<EditExperience />} />
-     </Routes>
+      <NavBar />
 
-     <Footer />
+      <Element className="Home">
+        <Header />
+      </Element>
+
+      <Element className="About">
+        <About />
+      </Element>
+
+      <Element className="Education">
+        <Education />
+      </Element>
+
+      <Element className="Projects">
+        <Projects />
+      </Element>
+
+      <Element className="Experience">
+        <Experience />
+      </Element>
+
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/admin" element={<Admin />} />
+      </Routes>
+      <Routes>
+        <Route path="/edit/:id" element={<EditAbout />} />
+        <Route path="/editEducation/:id" element={<EditEducation />} />
+        <Route path="/editProjects/:id" element={<EditProjects />} />
+        <Route path="/editExperience/:id" element={<EditExperience />} />
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
--- frontend/src/components/HomePages/NavBar.jsx
+++ frontend/src/components/HomePages/NavBar.jsx
@@ -1,48 +1,109 @@
-import React from 'react';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import styles from './styles/navbar.module.css';
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./styles/navbar.module.css";
+import { scroller } from "react-scroll";
 const NavBar = () => {
-    const [toggle,setToggle] = useState(false);
-    const actToggle = ()=>{
-        setToggle(!toggle);
+  const [toggle, setToggle] = useState(false);
+  const actToggle = () => {
+    setToggle(!toggle);
+  };
+  const closeNavBar = () => {
+    if (toggle === true) {
+      setToggle(false);
     }
-    const closeNavBar = ()=>{
-        if(toggle === true){
-            setToggle(false);
-        }
-    }
-    return ( 
+  };
+  const scrollToElement = (element) => {
+    scroller.scrollTo(element, {
+      duration: 800,
+      delay: 50,
+      smooth: true,
+      offset: -80,
+    });
+  };
+  return (
     <div>
-        <div className={styles.navContainer}>
-            <nav>
-                <div className={styles.logoBtn}>
-                    <Link to='/'>
-                        <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className={styles.logoImg} alt="logo" />
-                    </Link>
-                    <div className={styles.btn} onClick={actToggle}>
-                        <div className={toggle?`${styles.bar1} ${styles.animatedBar}`:`${styles.bar} ${styles.bar1}`}></div>
-                        <div className={toggle?`${styles.bar2} ${styles.animatedBar}`:`${styles.bar} ${styles.bar2}`}></div>
-                        <div className={toggle?`${styles.bar3} ${styles.animatedBar}`:`${styles.bar} ${styles.bar3}`}></div>
-                    </div>
-                </div>
-                <div className={styles.linksContainer}>
-                    <ul className={toggle?`${styles.newLinks} ${styles.links}`:styles.links} onClick={closeNavBar}>
-                            <li><Link to='/' >Home</Link></li> 
-                            <li><Link to='/' >About</Link></li> 
-                            <li><Link to='/' >Education</Link></li> 
-                            <li><Link to='/' >Experience</Link></li> 
-                            <li><Link to='/' >Projects</Link></li> 
-                            <li><Link to='/' >Contact</Link></li> 
-                            <li><Link to='/' >Home</Link></li> 
-                            <li className={styles.adminLi} ><Link to='/' >Admin</Link></li> 
-                            <li><Link to='/login' >Login</Link></li> 
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    </div> 
-    );
-}
- 
+      <div className={styles.navContainer}>
+        <nav>
+          <div className={styles.logoBtn}>
+            <Link to="/" onClick={() => scrollToElement("Home")}>
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+                className={styles.logoImg}
+                alt="logo"
+              />
+            </Link>
+            <div className={styles.btn} onClick={actToggle}>
+              <div
+                className={
+                  toggle
+                    ? `${styles.bar1} ${styles.animatedBar}`
+                    : `${styles.bar} ${styles.bar1}`
+                }
+              ></div>
+              <div
+                className={
+                  toggle
+                    ? `${styles.bar2} ${styles.animatedBar}`
+                    : `${styles.bar} ${styles.bar2}`
+                }
+              ></div>
+              <div
+                className={
+                  toggle
+                    ? `${styles.bar3} ${styles.animatedBar}`
+                    : `${styles.bar} ${styles.bar3}`
+                }
+              ></div>
+            </div>
+          </div>
+          <div className={styles.linksContainer}>
+            <ul
+              className={
+                toggle ? `${styles.newLinks} ${styles.links}` : styles.links
+              }
+              onClick={closeNavBar}
+            >
+              <li>
+                <Link to="/" onClick={() => scrollToElement("Home")}>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/" onClick={() => scrollToElement("About")}>
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to="/" onClick={() => scrollToElement("Education")}>
+                  Education
+                </Link>
+              </li>
+              <li>
+                <Link to="/" onClick={() => scrollToElement("Experience")}>
+                  Experience
+                </Link>
+              </li>
+              <li>
+                <Link to="/" onClick={() => scrollToElement("Projects")}>
+                  Projects
+                </Link>
+              </li>
+              {/* <li>
+                <Link  to="/"  onClick={() => scrollToElement("Home")}>Contact</Link>
+              </li> */}
+              <li className={styles.adminLi}>
+                <Link to="/">Admin</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
 export default NavBar;
