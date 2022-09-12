95300f897ef4c47a127a0141345ea603dc1dc8ac
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-12 10:58:45 +0530

    Rendering about component from API

--- frontend/src/components/HomePages/About.jsx
+++ frontend/src/components/HomePages/About.jsx
@@ -1,27 +1,43 @@
-import React from 'react';
-import styles from './styles/about.module.css'
+import React, { useEffect, useState } from "react";
+import styles from "./styles/about.module.css";
+import axios from "axios";
 const About = () => {
-    return ( 
+  const [about, setAbout] = useState();
+  useEffect(() => {
+    try {
+      const fetchAbout = async () => {
+        const response = (await axios.get("/about")).data;
+        console.log(response);
+        setAbout(response);
+      };
+      fetchAbout();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return (
     <div className={styles.mainContainer}>
-        <div className={styles.about}>
-            <div className={styles.aboutInfo}>
-            <h2 className={styles.title}>About</h2>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur 
-                    adipisicing elit. Natus nisi minus laudantium 
-                    cupiditate architecto debitis iusto. Consequatur 
-                    commodi eius repudiandae placeat perferendis ipsum 
-                    sapiente? Delectus est doloribus qui deleniti neque.
-                    Lorem ipsum, dolor sit amet consectetur 
-                    adipisicing elit. Natus nisi minus laudantium 
-                    cupiditate architecto debitis iusto. Consequatur 
-                    commodi eius repudiandae placeat perferendis ipsum 
-                    sapiente? Delectus est doloribus qui deleniti neque.
-                </p>
-            </div>
+      <div className={styles.about}>
+        <div className={styles.aboutInfo}>
+          <h2 className={styles.title}>About</h2>
+          {about.message.map((item) => (
+            <p>{item.about}</p>
+          ))}
+          {/* <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus nisi
+            minus laudantium cupiditate architecto debitis iusto. Consequatur
+            commodi eius repudiandae placeat perferendis ipsum sapiente?
+            Delectus est doloribus qui deleniti neque. Lorem ipsum, dolor sit
+            amet consectetur adipisicing elit. Natus nisi minus laudantium
+            cupiditate architecto debitis iusto. Consequatur commodi eius
+            repudiandae placeat perferendis ipsum sapiente? Delectus est
+            doloribus qui deleniti neque.
+          </p> */}
         </div>
-    </div> 
-    );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default About;
