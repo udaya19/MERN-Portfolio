4795992a8e30daa8219b1719e8c85b6967b55b61
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-12 09:07:41 +0530

    Setup context API

---
+++ frontend/src/components/context/globalContext.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import React, { createContext } from "react";
+export const DataContext = createContext();
+export const DataProvider = ({ children }) => {
+  return (
+    <DataContext.Provider value="hello from context">
+      {children}
+    </DataContext.Provider>
+  );
+};
