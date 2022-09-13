591f3ffef15380abc3ab8050fc5a4c03531c904c
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-13 09:51:13 +0530

    Added message in the backend for updating about

--- controllers/about_controller.js
+++ controllers/about_controller.js
@@ -1,72 +1,72 @@
-const About = require('../models/about');
+const About = require("../models/about");
 
-exports.getAbout = async (req,res)=>{
-    try {
-        const about = await About.find({});
-        return res.json(200,{
-            message:about
-        })
-    } catch (error) {
-        return res.json(500,{
-            message:error.message
-        })
-    }
-}
+exports.getAbout = async (req, res) => {
+  try {
+    const about = await About.find({});
+    return res.json(200, {
+      message: about,
+    });
+  } catch (error) {
+    return res.json(500, {
+      message: error.message,
+    });
+  }
+};
 
-exports.postAbout = async (req,res)=>{
-    try {
-        const {about} = req.body;
-        const newAbout = new About({about})
-        await newAbout.save();
-        return res.json(200,{
-            message:newAbout
-        })
-    } catch (error) {
-        return res.json(500,{
-            message:error.message
-        })
-    }
-}
+exports.postAbout = async (req, res) => {
+  try {
+    const { about } = req.body;
+    const newAbout = new About({ about });
+    await newAbout.save();
+    return res.json(200, {
+      message: newAbout,
+    });
+  } catch (error) {
+    return res.json(500, {
+      message: error.message,
+    });
+  }
+};
 
-exports.getSpecificAbout = async (req,res)=>{
-    try {
-        const about = await About.findById(req.params.id);
-        return res.json(200,{
-            about
-        })
-    } catch (error) {
-        return res.json(500,{
-            message:error.message
-        })
-    }
-}
+exports.getSpecificAbout = async (req, res) => {
+  try {
+    const about = await About.findById(req.params.id);
+    return res.json(200, {
+      about,
+    });
+  } catch (error) {
+    return res.json(500, {
+      message: error.message,
+    });
+  }
+};
 
-exports.updateAbout = async (req,res)=>{
-    try {
-        const {about} = req.body;
-        const newAbout = await About.findByIdAndUpdate(req.params.id,{about});
-        newAbout.save();
-        res.json(200,{
-            newAbout
-        })
-    } catch (error) {
-        res.json(500,{
-            message:error.message
-        })
-    }
+exports.updateAbout = async (req, res) => {
+  try {
+    const { about } = req.body;
+    const newAbout = await About.findByIdAndUpdate(req.params.id, { about });
+    newAbout.save();
+    res.json(200, {
+      message: "About us updated",
+      newAbout,
+    });
+  } catch (error) {
+    res.json(500, {
+      message: error.message,
+    });
+  }
+};
 
-}
-
-exports.deleteAbout = async (req,res)=>{
-   try {
+exports.deleteAbout = async (req, res) => {
+  try {
     const about = await About.findById(req.params.id);
     about.remove();
-    res.json(200,{
-     message:"About us line deleted"
-    })
-   } catch (error) {
-    res.json(500,{
-        message:error.message
-    })
-   }
-}
+    res.json(200, {
+      message: "About us line deleted",
+    });
+  } catch (error) {
+    res.json(500, {
+      message: error.message,
+    });
+  }
+};
