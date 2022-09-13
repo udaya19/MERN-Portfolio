b8c8990ffaa58626607c376206328ad10d1687ba
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-13 15:52:14 +0530

    Education component fetch data from API

--- frontend/src/components/HomePages/Education.jsx
+++ frontend/src/components/HomePages/Education.jsx
@@ -1,30 +1,47 @@
-import React from 'react';
-import styles from './styles/education.module.css'
+import React from "react";
+import styles from "./styles/education.module.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 const Education = () => {
-    return ( 
+  const [education, setEducation] = useState();
+  useEffect(() => {
+    const fetchEducation = async () => {
+      try {
+        const response = (await axios.get("/education")).data;
+        console.log(response);
+        setEducation(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchEducation();
+  }, []);
+  return (
     <div className={styles.mainContainer}>
-        <div className={styles.education}>
-            <h2 className={styles.title}>Education</h2>
-            <div className={styles.educationCenter}>
-                <div className={styles.singleEducation}>
-                    <p>
-                        Secondary High School- NSM Public School Vijayawada Andhra Pradesh
-                    </p>
-                </div>
-                <div className={styles.singleEducation}>
-                    <p>
-                        Senior Secondary High School- Happy Valley School Agiripalli Andhra Pradesh
-                    </p>
-                </div>
-                <div className={styles.singleEducation}>
-                    <p>
-                        B.Tech-Computer Science Engineering from BML Munjal University
-                    </p>
-                </div>
-            </div>
+      <div className={styles.education}>
+        <h2 className={styles.title}>Education</h2>
+        <div className={styles.educationCenter}>
+          <div className={styles.singleEducation}>
+            {education?.message.map((item) => (
+              <p key={item._id}>{item.education}</p>
+            ))}
+          </div>
+          {/* <div className={styles.singleEducation}>
+            <p>
+              Senior Secondary High School- Happy Valley School Agiripalli
+              Andhra Pradesh
+            </p>
+          </div>
+          <div className={styles.singleEducation}>
+            <p>
+              B.Tech-Computer Science Engineering from BML Munjal University
+            </p>
+          </div> */}
         </div>
-    </div> 
-    );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Education;
