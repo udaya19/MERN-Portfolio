1e650dc0204bf12414491742f2ea379d801932f2
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-14 10:25:29 +0530

    Add experience data from admin using API

--- frontend/src/components/AdminComponents/ExperienceAdmin.jsx
+++ frontend/src/components/AdminComponents/ExperienceAdmin.jsx
@@ -3,32 +3,43 @@ import styles from "./styles/admin.module.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
 const ExperienceAdmin = () => {
+  const [experienceData, setExperienceData] = useState();
   const [experience, setExperience] = useState();
   useEffect(() => {
     const fetchExperienceData = async () => {
       try {
         const response = (await axios.get("/experience")).data;
         console.log(response);
-        setExperience(response);
+        setExperienceData(response);
       } catch (error) {
         console.log(error);
       }
     };
     fetchExperienceData();
   }, []);
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const response = await axios.post("/experience/add", { experience });
+      console.log(response);
+      window.location.reload(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className={styles.sameComponent}>
       <div className={styles.sameForm}>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <h4>Experience component</h4>
           <label htmlFor="experience">Experience</label>
-          <input type="text" />
+          <input type="text" onChange={(e) => setExperience(e.target.value)} />
           <button type="submit">Add Item</button>
         </form>
       </div>
       <div className={styles.sameItem}>
-        {experience?.message.map((item) => (
-          <div className={styles.aboutInfo}>
+        {experienceData?.message.map((item) => (
+          <div className={styles.aboutInfo} key={item._id}>
             <div className={styles.sameAdmin}>
               <div className={styles.icons}>
                 <Link to="/editExperience">
