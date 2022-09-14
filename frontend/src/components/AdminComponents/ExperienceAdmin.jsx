8e440d7fda4f024cba8567539a005e2fd10d1dfc
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-14 09:54:58 +0530

    fetch data in experience admin component using api

--- frontend/src/components/AdminComponents/ExperienceAdmin.jsx
+++ frontend/src/components/AdminComponents/ExperienceAdmin.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./styles/admin.module.css";
+import axios from "axios";
 import { Link } from "react-router-dom";
 const ExperienceAdmin = () => {
+  const [experience, setExperience] = useState();
+  useEffect(() => {
+    const fetchExperienceData = async () => {
+      try {
+        const response = (await axios.get("/experience")).data;
+        console.log(response);
+        setExperience(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchExperienceData();
+  }, []);
   return (
     <div className={styles.sameComponent}>
       <div className={styles.sameForm}>
@@ -13,27 +27,26 @@ const ExperienceAdmin = () => {
         </form>
       </div>
       <div className={styles.sameItem}>
-        <div className={styles.aboutInfo}>
-          <div className={styles.sameAdmin}>
-            <div className={styles.icons}>
-              <Link to="/editExperience">
-                <span className={styles.editIcon}>
-                  <i className="fas fa-edit"></i>
+        {experience?.message.map((item) => (
+          <div className={styles.aboutInfo}>
+            <div className={styles.sameAdmin}>
+              <div className={styles.icons}>
+                <Link to="/editExperience">
+                  <span className={styles.editIcon}>
+                    <i className="fas fa-edit"></i>
+                  </span>
+                </Link>
+                <span className={styles.deleteIcon}>
+                  <i className="fas fa-trash"></i>
                 </span>
-              </Link>
-              <span className={styles.deleteIcon}>
-                <i className="fas fa-trash"></i>
-              </span>
+              </div>
+              <div className={styles.singleExperience}>
+                <p>{item.experience}</p>
+              </div>
+              {/* <h3 className={styles.itemDeleteTab}></h3> */}
             </div>
-            <div className={styles.singleExperience}>
-              <p>
-                Worked as a full stack developer intern at Spotflock
-                Technologies, Hyderabad from 30th May 2022 to 15th July 2022.
-              </p>
-            </div>
-            {/* <h3 className={styles.itemDeleteTab}></h3> */}
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
