48096bbc702fa888f7df55427aeb7d65fabdd6aa
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-14 09:47:13 +0530

    Experience component fetch data using API

--- frontend/src/components/HomePages/Experience.jsx
+++ frontend/src/components/HomePages/Experience.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./styles/experience.module.css";
+import axios from "axios";
 const Experience = () => {
+  const [experience, setExperience] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = (await axios.get("/experience")).data;
+      console.log(response);
+      setExperience(response);
+    };
+    fetchData();
+  }, []);
   return (
     <div className={styles.mainContainer}>
       <div className={styles.experience}>
         <h2 className={styles.title}>Experience</h2>
-        <div className={styles.experienceInfo}>
-          <div className={styles.experienceInfoCompanyName}>
-            <p className={styles.experienceInfoDetails}>
-              Worked as a full stack developer intern at Spotflock Technologies,
-              Hyderabad from 30th May 2022 to 15th July 2022.
-            </p>
+        {experience?.message.map((item) => (
+          <div className={styles.experienceInfo} key={item._id}>
+            <div className={styles.experienceInfoCompanyName}>
+              <p className={styles.experienceInfoDetails}>{item.experience}</p>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
