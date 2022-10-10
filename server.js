c848d661e135b5d7a6e2260f502938d2c1899fee
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-10 17:13:34 +0530

    Changed port number to 8000

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
-const express = require('express');
-const { urlencoded } = require('express');
-const env = require('dotenv').config();
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const { urlencoded } = require("express");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 //mongodb connection
 mongoose.connect(process.env.MONGO_URL);
 const db = mongoose.connection;
-db.on('error',console.error.bind(console,"Error in connecting database"));
-db.once('open',()=>console.log("Connected to database"));
+db.on("error", console.error.bind(console, "Error in connecting database"));
+db.once("open", () => console.log("Connected to database"));
 //Using middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(urlencoded({extended:true}))
+app.use(urlencoded({ extended: true }));
 app.use(cors());
-app.use(fileUpload({
-    useTempFiles:true
-}));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 //Using routes
-app.use('/user',require('./routes/user'));
-app.use('/education',require('./routes/education'));
-app.use('/experience',require('./routes/experience'));
-app.use('/projects',require('./routes/project'));
-app.use('/upload',require('./routes/upload'));
-app.use('/',require('./routes/about'));
+app.use("/user", require("./routes/user"));
+app.use("/education", require("./routes/education"));
+app.use("/experience", require("./routes/experience"));
+app.use("/projects", require("./routes/project"));
+app.use("/upload", require("./routes/upload"));
+app.use("/", require("./routes/about"));
 //Listening on port
-app.listen(process.env.PORT,()=>{
-    console.log("Listening on port 5000");
-})
+const port = 8000;
+app.listen(port, () => {
+  console.log("Listening on port", port);
+});
